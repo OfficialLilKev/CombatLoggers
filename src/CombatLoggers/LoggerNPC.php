91a53dca71293f985b57801a6a3df1d36a5e8f79
Lil Kev <kjolly544@gmail.com>
2025-07-23 19:40:30 -0400

    EARLY STAGES

--- src/CombatLoggers/LoggerNPC.php
+++ src/CombatLoggers/LoggerNPC.php
@@ -5,27 +5,27 @@ namespace CombatLogger;
 use pocketmine\entity\Human;
 use pocketmine\entity\Location;
 use pocketmine\item\Item;
-use pocketmine\player\Player;
-use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 use pocketmine\entity\Skin;
-use pocketmine\entity\AttributeFactory;
 
 class LoggerNPC extends Human {
 
     private string $originalName;
 
+    /**
+     * @param Location $location
+     * @param string $name
+     * @param Item[] $inventoryItems
+     */
     public function __construct(Location $location, string $name, array $inventoryItems) {
-        $skin = new Skin("Standard_Custom", str_repeat("00", 8192));
+        $skin = new Skin("Standard_Custom", str_repeat("\x00", 8192));
         parent::__construct($location, $skin);
+        
         $this->originalName = $name;
         $this->setNameTag("Combat Logger: " . $name);
         $this->setNameTagAlwaysVisible();
+
         foreach ($inventoryItems as $item) {
             $this->getInventory()->addItem($item);
         }
-    }
-
-    public static function getNetworkTypeId(): string {
-        return EntityIds::HUMAN;
     }
 }
